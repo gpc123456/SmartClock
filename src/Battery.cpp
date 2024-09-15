b04d4c252dd1f6e776126766f1c89a7a68792585
gpc <gpc731@qq.com>
2024-09-15 11:03:20 +0800

    由于硬件设计改动，更新电池电量计算方法

--- src/Battery.cpp
+++ src/Battery.cpp
@@ -1,23 +1,27 @@
 #include "Battery.h"
 
+// 当前电量计算模式使用100K与10K电阻进行分压
+// 电池电压为: U(ADC)/0.0909
+
 short ReadBatteryLevel()
 {
-    short BatteryVoltage;
     pinMode(A0, INPUT);
-    BatteryVoltage = analogRead(A0);
-    if (BatteryVoltage >= 738)
+    float BatteryPinAnalogRead = analogRead(A0);
+    short BatteryVoltage= ((1000.0/1024.0)*BatteryPinAnalogRead)/0.0909;
+    Serial.println(BatteryVoltage);
+    if (BatteryVoltage >= 3900)
     {
         return 4;
     }
-    else if (BatteryVoltage >= 710)
+    else if (BatteryVoltage >= 3800)
     {
         return 3;
     }
-    else if (BatteryVoltage >= 666)
+    else if (BatteryVoltage >= 3700)
     {
         return 2;
     }
-    else if (BatteryVoltage >= 625)
+    else if (BatteryVoltage >= 3400)
     {
         return 1;
     }
